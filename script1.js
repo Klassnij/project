48a6374aacaaa610ef863e187b53fb2d1168dc7a
Denis <klassnij33@mail.ru>
2022-11-06 18:48:13 +0300

    first commit

---
+++ script1.js
@@ -0,0 +1,32 @@
+"use strict"
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Den";
+
+alert(`Привет, ${user}`);
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(++decr);
+
+console.log(5%2);
+
+console.log(4 + 4 == '8');
+
+
+const isChecked = true,
+      isClose = false;
+
+
+console.log(isChecked || isClose);
+
+
+     
